4374da69d716114a7afe85a17800b2cfab0929f6
LessonDev <fl.lesson.dev@gmail.com>
2021-03-07 19:58:39 +0100

    classes, heritage et méthode abstraite

--- libraries/models/Article.php
+++ libraries/models/Article.php
@@ -4,53 +4,5 @@ require_once './libraries/models/Model.php';
 
 class Article extends Model
 {
-    /**
-     * Retourn la liste des articles classés par date de création
-     *
-     * @return array
-     */
-    public function findAll(): array
-    {
-        // On utilisera ici la méthode query (pas besoin de préparation car aucune variable n'entre en jeu)
-        $resultats = $this->pdo->query(
-            'SELECT * FROM articles ORDER BY created_at DESC'
-        );
-        // On fouille le résultat pour en extraire les données réelles
-        $articles = $resultats->fetchAll();
-
-        return $articles;
-    }
-
-    /**
-     * Retourne un article grace a son identifiant
-     *
-     * @param integer $id
-     * @return void
-     */
-    public function find(int $id)
-    {
-        $query = $this->pdo->prepare(
-            'SELECT * FROM articles WHERE id = :article_id'
-        );
-
-        // On exécute la requête en précisant le paramètre :article_id
-        $query->execute(['article_id' => $id]);
-
-        // On fouille le résultat pour en extraire les données réelles de l'article
-        $article = $query->fetch();
-
-        return $article;
-    }
-
-    /**
-     * Supprime un article dans la base grace a son identifiant
-     *
-     * @param integer $id
-     * @return void
-     */
-    public function delete(int $id): void
-    {
-        $query = $this->pdo->prepare('DELETE FROM articles WHERE id = :id');
-        $query->execute(['id' => $id]);
-    }
+    protected $table = 'articles';
 }
--- libraries/models/Comment.php
+++ libraries/models/Comment.php
@@ -4,6 +4,8 @@ require_once './libraries/models/Model.php';
 
 class Comment extends Model
 {
+    protected $table = 'comments';
+
     /**
      * Retouve la liste des commentaires d'un article donné
      *
@@ -20,33 +22,6 @@ class Comment extends Model
         $commentaires = $query->fetchAll();
 
         return $commentaires;
-    }
-
-    /**
-     * Retourne un commentaire de la base de données grace a son identifiant
-     *
-     * @param integer $id
-     * @return array/bool le commentaire si on le trouve, false si on ne le trouve pas
-     */
-    function find(int $id)
-    {
-        $query = $this->pdo->prepare('SELECT * FROM comments WHERE id = :id');
-        $query->execute(['id' => $id]);
-        $comment = $query->fetch();
-
-        return $comment;
-    }
-
-    /**
-     * Supprime un commentaire grace a son identifiant
-     *
-     * @param integer $id
-     * @return void
-     */
-    public function delete(int $id): void
-    {
-        $query = $this->pdo->prepare('DELETE FROM comments WHERE id = :id');
-        $query->execute(['id' => $id]);
     }
 
     /**
